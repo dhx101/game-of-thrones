78215f2309b785c82ad47c9825518dfdcfedb7bf
alberto-baquero <baquero.llopis.alberto@gmail.com>
2024-01-30 17:05:15 +0100

    mapeo de Api

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
-}
-
+};
