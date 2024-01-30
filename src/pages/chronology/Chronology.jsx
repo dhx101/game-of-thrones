290e97fc5bfeb62826b29fe17eda64181027efa8
David <138037766+dhx101@users.noreply.github.com>
2024-01-30 16:54:46 +0100

    added button to change order

--- src/pages/chronology/Chronology.jsx
+++ src/pages/chronology/Chronology.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { ApiContext } from "../../context/Context";
 
 const Chronology = () => {
-  return (
-    <div>
-      Chronology
-    </div>
-  )
-}
+	const { characters } = useContext(ApiContext);
+	const [ordenatedList, setOrdenatedList] = useState(characters);
+    const [ascending, setAscending] = useState(true)
+	console.log(ordenatedList);
+	const cambiarOrden = () => {
+		setAscending(!ascending)
+	};
 
-export default Chronology
+	const ordenar = () => {
+        cambiarOrden()
+		const newArray = [...characters];
+		newArray.sort((a, b) => {
+			const orden = ascending ? 1 : -1;
+			return orden * (a.age - b.age);
+		});
+		setOrdenatedList(newArray);
+        console.log(ascending);
+	};
+
+	useEffect(() => {}, []);
+
+	return (
+		<>
+			<button onClick={ordenar}>O</button>
+			{ordenatedList.map((item, index) => (
+				<div key={index}>
+					<p>{item.age}</p>
+					<p>{item.name}</p>
+					<img src={item.image} alt={item.name}></img>
+				</div>
+			))}
+		</>
+	);
+};
+
+export default Chronology;
