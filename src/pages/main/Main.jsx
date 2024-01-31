0fb40f93617cb22da23720906bd5540b4adf4af4
alberto-baquero <baquero.llopis.alberto@gmail.com>
2024-01-31 22:02:37 +0100

    Link a los personajes

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -7,6 +7,7 @@ import Chronology from "../chronology/Chronology";
 import Links from "../../components/nav/Links";
 import DetailHouses from "../detailHouses/DetailHouses";
 import Lang from "../../components/lang/Lang";
+import CharactersDetails from "../charactersDetails/CharacterDetails"
 
 const Main = () => {
 	return (
@@ -16,6 +17,7 @@ const Main = () => {
 				<Routes>
 					<Route path="/" element={<Inicio />} />
 					<Route path="/characters" element={<Characters />} />
+					<Route path="/characterDetails/:id" element={<CharactersDetails />} />
 					<Route path="/houses" element={<Houses />} />
 					<Route path="/chronology" element={<Chronology />} />
 					<Route path="/DetailHouses/:id" element={<DetailHouses />} />
