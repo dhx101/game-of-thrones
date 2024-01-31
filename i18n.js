5efbb9dffc7fd804269eae184d6b69af5817e5cd
David <138037766+dhx101@users.noreply.github.com>
2024-01-31 19:47:50 +0100

    i18next

---
+++ i18n.js
@@ -0,0 +1,14 @@
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+
+const resources = {
+    en: {
+        translation: {
+            "Welcome to React": "Welcome to React and react-i18next" 
+        }
+    },
+    
+}
+
+
+export default i18n;
