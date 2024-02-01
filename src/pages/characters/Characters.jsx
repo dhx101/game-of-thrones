d395ee08f3c8278ac5c8b01eb11f641513681199
David <138037766+dhx101@users.noreply.github.com>
2024-02-01 17:39:34 +0100

    changed header

--- src/pages/characters/Characters.jsx
+++ src/pages/characters/Characters.jsx
@@ -1,12 +1,46 @@
 // import React, { useContext } from "react";
 // import { ApiContext } from "../../context/Context";
 import { Link } from "react-router-dom";
+import SearchBar from "../../components/searchbar/SearchBar";
+import Lang from "../../components/lang/Lang";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Characters = ({characters}) => {
+
+
+const Characters = () => {
   // const { characters } = useContext(ApiContext);
+  const baseURL = "http://localhost:3000";
+	const [characters, setCharacters] = useState([]);
 
+	const getCharacters = async (name) => {
+		try {
+			if (name) {
+				const charactersApi = await axios.get(
+					`${baseURL}/characters?name=${name}`
+				);
+				setCharacters(charactersApi.data);
+			} else {
+				const charactersApi = await axios.get(`${baseURL}/characters`);
+				setCharacters(charactersApi.data); // Extraer los datos de la respuesta
+			}
+		} catch (error) {
+			console.error("Error fetching characters:", error);
+		}
+	};
+	useEffect(() => {
+		getCharacters();
+	}, []);
+
+	const urlJson = (name) => {
+		getCharacters(name);
+	};
   return (
     <>
+    <header className="header">
+        <SearchBar urlJson={urlJson} />
+        <Lang />
+      </header>
       <div className="character">
         {characters.map((Character) => (
           <div className="character-list" key={Character.id}>
--- src/pages/detailHouses/DetailHouses.jsx
+++ src/pages/detailHouses/DetailHouses.jsx
@@ -1,63 +1,79 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import axios from "axios";
-import "./DetailHouses.scss";
+import Lang from "../../components/lang/Lang";
+import { Link } from "react-router-dom";
 
 const baseURL = "http://localhost:3000";
 
 function DetailHouses() {
-  const { id } = useParams();
-  const [detailHouses, setDetailouses] = useState([]);
+	const { id } = useParams();
+	const [detailHouses, setDetailouses] = useState([]);
 
-  useEffect(() => {
-    const getdetailHouses = async () => {
-      try {
-        const detailHousesApi = await axios.get(`${baseURL}/houses/${id}`);
-        setDetailouses(detailHousesApi.data); // Extraer los datos de la respuesta
-      } catch (error) {
-        console.error("Error fetching houses:", error);
-      }
-    };
-    getdetailHouses();
-  }, [id]);
+	useEffect(() => {
+		const getdetailHouses = async () => {
+			try {
+				const detailHousesApi = await axios.get(`${baseURL}/houses/${id}`);
+				setDetailouses(detailHousesApi.data); // Extraer los datos de la respuesta
+			} catch (error) {
+				console.error("Error fetching houses:", error);
+			}
+		};
+		getdetailHouses();
+	}, [id]);
 
-  return (
-    <div className="fondo1">
-      <div className="setImage">
-        <img className="fondo1img1" src={detailHouses.image} alt="bandera" />
-      </div>
-
-      <div className="fondo1name">
-        <h1 className="fondo2name">House {detailHouses.name}</h1>
-      </div>
-      <div className="fondo1__titles1">
-        <div className="fondo1__pro1">
-          <h1 className="fondo1__titulos">SEDE</h1>
-          <p className="fondo1__propiedades">{detailHouses.settlement}</p>
-        </div>
-        <div className="fondo1pro1">
-          <h1 className="fondo1__titulos">REGION</h1>
-          <p className="fondo1__propiedades">{detailHouses.region}</p>
-        </div>
-        <div className="fondo1pro1">
-          <h1 className="fondo1__titulos">ALIANZAS</h1>
-          <div className="p1">
-          {detailHouses?.alliances?.map((house, index) => (
-            <p key={index}>{house}</p>
-          ))}
-          </div>
-        </div>
-        <div className="fondo1pro1">
-          <h1 className="fondo1__titulos">RELIGICIONES</h1>
-          <p className="fondo1__propiedades">{detailHouses.religions}</p>
-        </div>
-        <div className="fondo1__pro1">
-          <h1 className="fondo1__titulos">FUNDACION</h1>
-          <p className="fondo1__propiedades">{detailHouses.foundation}</p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<header className="header">
+				<Link to={"/houses"}> Atras </Link>
+				<Lang />
+			</header>
+			<div className="fondo1">
+				<div className="setImage">
+					<img
+						className="fondo1img1"
+						src={detailHouses.image}
+						alt="bandera"
+					/>
+				</div>
+				<div className="fondo1name">
+					<h1 className="fondo2name">House {detailHouses.name}</h1>
+				</div>
+				<div className="fondo1__titles1">
+					<div className="fondo1__pro1">
+						<h1 className="fondo1__titulos">SEDE</h1>
+						<p className="fondo1__propiedades">
+							{detailHouses.settlement}
+						</p>
+					</div>
+					<div className="fondo1pro1">
+						<h1 className="fondo1__titulos">REGION</h1>
+						<p className="fondo1__propiedades">{detailHouses.region}</p>
+					</div>
+					<div className="fondo1pro1">
+						<h1 className="fondo1__titulos">ALIANZAS</h1>
+						<div className="p1">
+							{detailHouses?.alliances?.map((house, index) => (
+								<p key={index}>{house}</p>
+							))}
+						</div>
+					</div>
+					<div className="fondo1pro1">
+						<h1 className="fondo1__titulos">RELIGICIONES</h1>
+						<p className="fondo1__propiedades">
+							{detailHouses.religions}
+						</p>
+					</div>
+					<div className="fondo1__pro1">
+						<h1 className="fondo1__titulos">FUNDACION</h1>
+						<p className="fondo1__propiedades">
+							{detailHouses.foundation}
+						</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default DetailHouses;
