509167753cc53b44052f38c257a7fb59e449ada5
David <138037766+dhx101@users.noreply.github.com>
2024-02-02 09:45:16 +0100

    small changes

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "../inicio/Inicio";
 import Characters from "../characters/Characters";
@@ -5,27 +6,61 @@ import Houses from "../houses/Houses";
 import Chronology from "../chronology/Chronology";
 import Links from "../../components/nav/Links";
 import DetailHouses from "../detailHouses/DetailHouses";
+import Lang from "../../components/lang/Lang";
 import CharactersDetails from "../charactersDetails/CharacterDetails";
-
+import axios from "axios";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
 const Main = () => {
+	const baseURL = "http://localhost:3000";
+	const [characters, setCharacters] = useState([]);
+
+	const getCharacters = async (name) => {
+		try {
+			if (name) {
+				const charactersApi = await axios.get(
+					`${baseURL}/characters?name=${name}`
+				);
+				console.log(`${baseURL}/characters?name=${name}`);
+				setCharacters(charactersApi.data);
+			} else {
+				const charactersApi = await axios.get(`${baseURL}/characters`);
+				setCharacters(charactersApi.data); // Extraer los datos de la respuesta
+			}
+		} catch (error) {
+			console.error("Error fetching characters:", error);
+		}
+	};
+	useEffect(() => {
+		getCharacters();
+	}, []);
+
+	const urlJson = (name) => {
+		getCharacters(name);
+	};
+
 	return (
 		<>
 			<BrowserRouter>
-				
-				<Routes>
-					<Route path="/" element={<Inicio />} />
-					<Route
-						path="/characters"
-						element={<Characters/>}
-					/>
-					<Route
-						path="/characterDetails/:id"
-						element={<CharactersDetails />}
-					/>
-					<Route path="/houses" element={<Houses />} />
-					<Route path="/chronology" element={<Chronology />} />
-					<Route path="/detailHouses/:id" element={<DetailHouses />} />
-				</Routes>
+				<div>
+					<Lang urlJson={urlJson} />
+				</div>
+				<SimpleBar forceVisible="y" style={{ height: "80vh" }}>
+					<Routes>
+						<Route path="/" element={<Inicio />} />
+						<Route
+							path="/characters"
+							element={<Characters characters={characters} />}
+						/>
+						<Route
+							path="/characterDetails/:id"
+							element={<CharactersDetails />}
+						/>
+						<Route path="/houses" element={<Houses />} />
+						<Route path="/chronology" element={<Chronology />} />
+						<Route path="/DetailHouses/:id" element={<DetailHouses />} />
+					</Routes>
+				</SimpleBar>
 				<Links />
 			</BrowserRouter>
 		</>
